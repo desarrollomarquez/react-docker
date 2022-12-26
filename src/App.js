de1d6da30eed324b5c5db0dc88a7d066f98a3218
UruitDevops <UruitDevops>
2022-12-25 21:33:08 -0500

    Ajust test Image Docker

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvendo App Backend Test - Docker Image for AWS
+          Bienvendo App Backend Test - Docker Image for AWS...
         </p>
       </header>
     </div>
